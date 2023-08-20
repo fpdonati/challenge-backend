16f23cc203c22c559751a4a6c59b4fe8aacd1680
francodonati <fpdonati@uc.cl>
2023-08-20 13:08:45 -0400

    add start db uri

--- src/database/database.js
+++ src/database/database.js
@@ -1,11 +1,8 @@
 import Sequelize from "sequelize";
+import { config } from "dotenv";
 
-export const sequelize = new Sequelize(
-  "challenge",
-  "challenge_user",
-  "mypassword",
-  {
-    host: "localhost",
-    dialect: "postgres",
-  }
-);
+config();
+
+export const sequelize = new Sequelize(process.env.POSTGRESQL_DB_URI, {
+  dialect: "postgres",
+});
