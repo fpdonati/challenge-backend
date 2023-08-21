164c5d726436fd1e294631169cf6845d2c9648e1
francodonati <fpdonati@uc.cl>
2023-08-20 21:35:50 -0400

    add cors

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
 import express from "express";
 import postsRoutes from "./routes/posts.js";
-
+import cors from "cors";
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use(postsRoutes);
-
 
 export default app;
--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ import "./models/Post.js";
 async function main() {
   try {
     await sequelize.sync({ force: false });
-    app.listen(3000);
-    console.log("Server is listening on port: ", 3000);
+    app.listen(4000);
+    console.log("Server is listening on port: ", 4000);
   } catch (error) {
     console.error("Unable to connect", error);
   }
